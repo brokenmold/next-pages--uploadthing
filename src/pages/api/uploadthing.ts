3006450898f47238105d4fb06290b7602471c8d9
Jason Glass <jason@broken-mold.com>
2023-07-13 11:16:59 -0500

    Fix Paths

--- src/pages/api/uploadthing.ts
+++ src/pages/api/uploadthing.ts
@@ -1,6 +1,6 @@
 import { createNextPageApiHandler } from "uploadthing/next-legacy";
 
-import { ourFileRouter } from "~/server/uploadthing";
+import { ourFileRouter } from "@/server/uploadthing";
 
 const handler = createNextPageApiHandler({
   router: ourFileRouter,
